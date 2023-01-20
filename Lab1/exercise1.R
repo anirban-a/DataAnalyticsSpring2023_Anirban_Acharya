082b51a6bd7fd72d696ac608dd603878a6d76019
Anirban Acharya <acharya.anirban9@gmail.com>
2023-01-20 16:14:59 -0500

    added code to remove NA values

--- Lab1/exercise1.R
+++ Lab1/exercise1.R
@@ -1,1 +1,23 @@
-data <- read.csv("EPI2010_data.csv")
+install.packages("dplyr")
+install.packages("purr")
+
+library(dplyr)
+library(purrr)
+
+epi_data <- read.csv("EPI2010_data.csv")
+
+epi.treated <- epi_data
+
+epi.means<-1:ncol(epi.treated) %>% map(function(col_idx){
+  if(is.numeric(epi.treated[1,col_idx])){
+    mean(epi.treated[,col_idx], na.rm=TRUE)
+  }
+})
+
+for(i in 1:nrow(epi.treated)){
+  for(j in 1:ncol(epi.treated)){
+    if(is.na(epi.treated[i,j]) && is.numeric(epi.treated[i,j])){
+      epi.treated[i,j]<-epi.means[j]
+    }
+  }
+}
