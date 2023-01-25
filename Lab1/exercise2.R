90ba06b1fe5d2627a8f6fbdfe29651f9ead04bbb
Anirban Acharya <acharya.anirban9@gmail.com>
2023-01-24 23:09:25 -0500

    Added scatter plots for population density vs Desert

--- Lab1/exercise2.R
+++ Lab1/exercise2.R
@@ -9,7 +9,7 @@ library(ggplot2)
 
 ggplot(data = epi_data) +
   geom_point(mapping = aes(EPI, as.logical(No_surface_water), color=No_surface_water)) +
-  labs(y="No Surface Water", x="EPI")
+  labs(y="No Surface Water", x="EPI") + xlim(37,78.5)
 
 ggplot(data = epi_data) +
   geom_point(mapping = aes(as.logical(No_surface_water),GEO_subregion, color=No_surface_water)) +
@@ -18,3 +18,13 @@ ggplot(data = epi_data) +
 ggplot(data = epi_data) +
   geom_point(mapping = aes(as.logical(No_surface_water), EPI_regions, color=No_surface_water)) +
   labs(x="No Surface Water", y="EPI Regions")
+
+
+
+# Let's check the distribution of population density..
+boxplot(epi_data$PopulationDensity, notch = TRUE)
+
+# population density vs EPI, categorized by Desert:
+ggplot(data = epi_data) +
+  geom_point(mapping = aes(EPI, PopulationDensity))+
+  facet_wrap(~ as.logical(Desert))
